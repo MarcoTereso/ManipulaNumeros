67de04b9a7c9f37ddcc604bbbfd343e2d0a40af7
Marco Tereso <marco.tereso@gmail.com>
2022-09-20 15:02:25 +0100

    Media Aluno ex6

--- ManipulaNumeros/src/manipulanumeros/ManipulaNumeros.java
+++ ManipulaNumeros/src/manipulanumeros/ManipulaNumeros.java
@@ -18,6 +18,7 @@ public class ManipulaNumeros {
             System.out.println("3 - Soma impares multiplica pares");
             System.out.println("4 - Troca valores entre variáveis");
             System.out.println("5 - Compara números");
+            System.out.println("6 - Calcular média de aluno");
             
             System.out.println("0 - Sair");
             System.out.println("Selecione a opção pretendida");
@@ -42,6 +43,9 @@ public class ManipulaNumeros {
                 case 5:
                     leValores("Insira um valor","Insira outro valor");
                     ComparaNumero.comparar2Valores(x,y);
+                    break;
+                case 6:
+                    MediaAluno.registaNotas();
                     break;
                 default: System.out.println("Insira opção válida");
             }
